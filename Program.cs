4ad9f4a98d6a22c113701de62fcde1939f58a66a
rezonn <rezonn@msn.com>
2020-10-24 10:16:38 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,18 +11,14 @@ class Program
 
     static void Main(string[] args)
     {
-
         Dictionary<string, string> values = new Dictionary<string, string>();
-
-        // hopefully you have even number args count.
         for (int i = 0; i < args.Length; i += 2)
         {
             values.Add(args[i], args[i + 1]);
         }
-
         String xls = values["-xsl"];
         String sql = values["-sql"];
-
+        
         string sConnectionString = V + "Data Source=" + xls + ";Extended Properties=Excel 8.0;";
         OleDbConnection objConn = new OleDbConnection(sConnectionString);
         objConn.Open();
@@ -31,15 +27,12 @@ class Program
         DataSet objDataset1 = new DataSet();
         objAdapter1.Fill(objDataset1, "XLData");
         DataTable tblAuthors = objDataset1.Tables[0];
-
         String rowstr = "";
-
         foreach (DataColumn column in tblAuthors.Columns)
         {
             rowstr += column.ToString() + ";";
         }
         Console.WriteLine(rowstr.Substring(0, rowstr.Length - 1));
-
         foreach (DataRow drCurrent in tblAuthors.Rows)
         {
             rowstr = "";
