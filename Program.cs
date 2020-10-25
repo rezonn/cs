621648711a0e5ffa5cbdf94ab41a2c14c7c6d341
rezonn <rezonn@msn.com>
2020-10-25 09:21:44 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,43 +4,58 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.Xml.Xsl;
 
-class Program
+namespace ww
 {
-    private const string V = "Provider=Microsoft.Jet.OLEDB.4.0;";
-    static void Main(string[] args)
+    static class XLS
     {
-        Dictionary<string, string> values = new Dictionary<string, string>();
-        for (int i = 0; i < args.Length; i += 2)
+        public static string Query(string xls, string sql)
         {
-            values.Add(args[i], args[i + 1]);
-        }
-        String xls = values["-xsl"];
-        String sql = values["-sql"];
-        
-        string sConnectionString = V + "Data Source=" + xls + ";Extended Properties=Excel 8.0;";
-        OleDbConnection objConn = new OleDbConnection(sConnectionString);
-        objConn.Open();
-        OleDbDataAdapter objAdapter1 = new OleDbDataAdapter();
-        objAdapter1.SelectCommand = new OleDbCommand(sql, objConn);
-        DataSet objDataset1 = new DataSet();
-        objAdapter1.Fill(objDataset1, "XLData");
-        DataTable tblAuthors = objDataset1.Tables[0];
-        String rowstr = "";
-        foreach (DataColumn column in tblAuthors.Columns)
-        {
-            rowstr += column.ToString() + ";";
-        }
-        Console.WriteLine(rowstr.Substring(0, rowstr.Length - 1));
-        foreach (DataRow drCurrent in tblAuthors.Rows)
-        {
-            rowstr = "";
+            string V = "Provider=Microsoft.Jet.OLEDB.4.0;";
+            string sConnectionString = V + "Data Source=" + xls + ";Extended Properties=Excel 8.0;";
+            OleDbConnection objConn = new OleDbConnection(sConnectionString);
+            objConn.Open();
+            OleDbDataAdapter objAdapter1 = new OleDbDataAdapter();
+            objAdapter1.SelectCommand = new OleDbCommand(sql, objConn);
+            DataSet objDataset1 = new DataSet();
+            objAdapter1.Fill(objDataset1, "XLData");
+            DataTable tblAuthors = objDataset1.Tables[0];
+            String rv = "";
+            String rowstr = "";
             foreach (DataColumn column in tblAuthors.Columns)
             {
-                rowstr += drCurrent[column].ToString() + ";";
+                rowstr += column.ToString() + ";";
             }
-            Console.WriteLine(rowstr.Substring(0, rowstr.Length - 1));
+            rv += (rowstr.Substring(0, rowstr.Length - 1));
+            foreach (DataRow drCurrent in tblAuthors.Rows)
+            {
+                rowstr = "";
+                foreach (DataColumn column in tblAuthors.Columns)
+                {
+                    rowstr += drCurrent[column].ToString() + ";";
+                }
+                rv += "\n" + (rowstr.Substring(0, rowstr.Length - 1));
+            }
+            objConn.Close();
+            return rv;
         }
-        objConn.Close();
+    }
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                values.Add(args[i], args[i + 1]);
+            }
+            if (values.ContainsKey("-xsl") && values.ContainsKey("-sql"))
+            {
+                String xls = values["-xsl"];
+                String sql = values["-sql"];
+                Console.WriteLine(XLS.Query(xls,sql));
+            }
+        }
     }
 }
